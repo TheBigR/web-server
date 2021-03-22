42d0dd839bb6a5136fe171101048a52ee298ac24
TheBigR <nevoroy@gmail.com>
2021-03-22 06:16:57 +0200

    static files served

--- src/app.js
+++ src/app.js
@@ -1,21 +1,12 @@
+const path = require('path')
 const express = require('express')
 
 const app = express()
 
-app.get('', (req, res) => {
-  res.send('Hello express!')
-})
-
-app.get('/help', (req, res) => {
-  res.send('Help page')
-})
-
-app.get('/about', (req, res) => {
-  res.send('About page')
-})
+app.use(express.static(path.join(__dirname, '../public')))
 
 app.get('/weather', (req, res) => {
-  res.send('Weather page')
+  res.send({ temp: 20, feelsLike: 23 })
 })
 
 app.listen(3000, () => {

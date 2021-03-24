acb3954deeab4000e5bdb293afd109115ca0d52b
TheBigR <nevoroy@gmail.com>
2021-03-24 13:14:00 +0200

    added handlebars

--- src/app.js
+++ src/app.js
@@ -3,7 +3,29 @@ const express = require('express')
 
 const app = express()
 
+app.set('view engine', 'hbs')
 app.use(express.static(path.join(__dirname, '../public')))
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather app',
+    name: 'Roy Nevo',
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About',
+    name: 'Roy Nevo',
+  })
+})
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help',
+    message: 'This comes to help with your weather.',
+  })
+})
 
 app.get('/weather', (req, res) => {
   res.send({ temp: 20, feelsLike: 23 })

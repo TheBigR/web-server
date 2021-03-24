4dbbd4db0b99d8f03c691f1311fa4c2c91fce32f
TheBigR <nevoroy@gmail.com>
2021-03-24 13:24:25 +0200

    cosmetics

--- src/app.js
+++ src/app.js
@@ -3,8 +3,16 @@ const express = require('express')
 
 const app = express()
 
+//Define paths for Express config.
+const publicDirPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates')
+
+//Setup handlebars engine and views location
 app.set('view engine', 'hbs')
-app.use(express.static(path.join(__dirname, '../public')))
+app.set('views', viewsPath)
+
+//Setup static directory to serve
+app.use(express.static(publicDirPath))
 
 app.get('', (req, res) => {
   res.render('index', {

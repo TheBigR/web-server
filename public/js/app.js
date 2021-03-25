6060e2ef50f5e795fae79b3c1089bea1756e13c9
TheBigR <nevoroy@gmail.com>
2021-03-25 15:56:02 +0200

    search form created

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,21 @@
 console.log('Client side js file loaded')
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const location = search.value
+  fetch('http://localhost:3000/weather?address=' + location).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          console.log('error ' + data.error)
+        } else {
+          console.log('location: ' + data.location)
+          console.log('forecast: ' + data.forecastData)
+        }
+      })
+    },
+  )
+})

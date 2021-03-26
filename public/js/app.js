ceb14f6c3867a274959974332e83006da6ffc564
TheBigR <nevoroy@gmail.com>
2021-03-27 00:08:07 +0300

    css updated

--- public/js/app.js
+++ public/js/app.js
@@ -2,18 +2,22 @@ console.log('Client side js file loaded')
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
 
 weatherForm.addEventListener('submit', (e) => {
   e.preventDefault()
   const location = search.value
+  messageOne.textContent = 'Loading...'
+  messageTwo.textContent = ''
   fetch('http://localhost:3000/weather?address=' + location).then(
     (response) => {
       response.json().then((data) => {
         if (data.error) {
-          console.log('error ' + data.error)
+          messageOne.textContent = data.error
         } else {
-          console.log('location: ' + data.location)
-          console.log('forecast: ' + data.forecastData)
+          messageOne.textContent = 'location: ' + data.location
+          messageTwo.textContent = 'forecast: ' + data.forecastData
         }
       })
     },

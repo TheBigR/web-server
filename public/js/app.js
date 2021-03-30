0ed71fc1db0f5bb83ebd6f4a86906daf554f2bbd
TheBigR <nevoroy@gmail.com>
2021-03-30 15:35:38 +0300

    changes pre prod

--- public/js/app.js
+++ public/js/app.js
@@ -10,16 +10,14 @@ weatherForm.addEventListener('submit', (e) => {
   const location = search.value
   messageOne.textContent = 'Loading...'
   messageTwo.textContent = ''
-  fetch('http://localhost:3000/weather?address=' + location).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          messageOne.textContent = data.error
-        } else {
-          messageOne.textContent = 'location: ' + data.location
-          messageTwo.textContent = 'forecast: ' + data.forecastData
-        }
-      })
-    },
-  )
+  fetch('/weather?address=' + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error
+      } else {
+        messageOne.textContent = 'location: ' + data.location
+        messageTwo.textContent = 'forecast: ' + data.forecastData
+      }
+    })
+  })
 })
